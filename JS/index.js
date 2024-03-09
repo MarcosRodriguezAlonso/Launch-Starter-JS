b06c2e3eb0f085a977ee761e011f4badae2b459d
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-09 12:46:20 +0100

    añadidas expresiones literales a "passwordCheck".

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,20 @@
-const name = "Marcos";
+const name = "Pep";
 const password = "CalleFalsa123";
 const passwordConfirmation = "CalleFalsa123";
-const age = 29;
+const age = 17;
 
 const nameCheck = name.length >= 5;
-const passwordNumbersCheck = password.includes(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-const passwordLengthCheck = password.length >= 8;
+const passwordCheck =
+  /.{8,}/.test(password) &&
+  /(?:[A-Z])/.test(password) &&
+  /(?:[a-z])/.test(password) &&
+  /(?:\d)/.test(password);
 const passwordMatchCheck = password === passwordConfirmation;
 const ageCheck = age >= 18;
 
 console.log(`El nombre de usuario es válido : ${nameCheck}`);
 
-console.log(
-  `La contraseña es válida: ${passwordNumbersCheck}${passwordLengthCheck}`
-);
+console.log(`La contraseña es válida: ${passwordCheck}`);
 
 console.log(`La repetición de contraseña es válida: ${passwordMatchCheck}`);
 
