245e6e89b261296e919f2e6eb1f761bfa2786d2e
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-12 13:54:17 +0100

    update "const hasPasswordRequirements"

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,19 @@
 const name = "Pep";
-const password = "Cáñamo123";
-const passwordConfirmation = "Cáñamo123";
+const password = "CÁñamo123";
+const passwordConfirmation = "CÁñamo123";
 const age = 17;
 
 const hasNameRequiredLength = name.length >= 5;
 const hasPasswordRequirements =
   password.length >= 8 &&
-  /([A-Z])/.test(password) &&
   /(\d)/.test(password) &&
-  /([a-zA-Z0-9\sÁÉÍÓÚáéíóúÑñ])/.test(password);
-const IsBothPasswordsEqual = password === passwordConfirmation;
-const IsAdult = age >= 18;
+  password.toLowerCase() !== password;
+const areBothPasswordsEqual = password === passwordConfirmation;
+const isAdult = age >= 18;
 
 console.log(`
-El nombre de usuario es válido : ${hasNameRequiredLength}
+El nombre de usuario es válido: ${hasNameRequiredLength}
 La contraseña es válida: ${hasPasswordRequirements}
-La repetición de contraseña es válida: ${IsBothPasswordsEqual}
-Es mayor de edad: ${IsAdult}
+La repetición de contraseña es válida: ${areBothPasswordsEqual}
+Es mayor de edad: ${isAdult}
 `);
